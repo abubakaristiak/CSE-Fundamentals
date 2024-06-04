e8c2948410b3b8f42e3d5014c61477b258513fb7
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-06-04 13:11:43 +0600

    modify program

--- C++/Module - 3.5/_05_O_Sort_String.cpp
+++ C++/Module - 3.5/_05_O_Sort_String.cpp
@@ -1,25 +1,40 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// void bubblesort(string &s, int n){
+//     for(int i=0; i<n-1; i++){
+//         for(int j=0; j=n-i-1; j++){
+//             if(s[j] > s[j+1]){
+//                 char temp = s[j];
+//                 s[j] = s[j+1];
+//                 s[j+1] = temp;
+//             }
+//         }
+//     }
+// }
+// int main()
+// {
+//     int n;
+//     cin >> n;
+
+//     string s;
+//     cin >> s;
+//     bubblesort(s,n);
+
+//     cout << s << endl;
+//     return 0;
+// }
+
+//https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/O
 #include<bits/stdc++.h>
 using namespace std;
-void bubblesort(string &s, int n){
-    for(int i=0; i<n-1; i++){
-        for(int j=0; j=n-i-1; j++){
-            if(s[j] > s[j+1]){
-                char temp = s[j];
-                s[j] = s[j+1];
-                s[j+1] = temp;
-            }
-        }
-    }
-}
 int main()
 {
     int n;
-    cin >> n;
-
-    string s;
-    cin >> s;
-    bubblesort(s,n);
-
-    cout << s << endl;
+    cin>>n;
+    string s[n];
+    for(int i=0; i<n; i++){
+        cout << s[i];
+    }
+    
     return 0;
 }
