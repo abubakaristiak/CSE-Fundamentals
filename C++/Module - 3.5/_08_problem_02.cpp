16f5219d6b6f0551af6404c933ec1a6327520991
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-06-05 22:49:29 +0600

    add program

--- C++/Module - 3.5/_08_problem_02.cpp
+++ C++/Module - 3.5/_08_problem_02.cpp
@@ -1,12 +1,26 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// class Cricketer
-// {
-//     int jersey_no
-//     char country;
-// }
-// int main()
-// {
-//     int* dhoni = new int;
-//     return 0;
-// }
+#include<bits/stdc++.h>
+using namespace std;
+class Cricketer
+{
+    public:
+        int jersey_no;
+        string country;
+};
+int main()
+{
+    Cricketer* dhoni = new Cricketer;
+    dhoni->jersey_no = 10;
+    dhoni->country = "India";
+
+    Cricketer* kohli = new Cricketer;
+    kohli->jersey_no = dhoni->jersey_no;
+    kohli->country = dhoni->country;
+
+    cout << kohli->jersey_no << endl;
+    cout << kohli->country << endl;
+
+    delete dhoni;
+    delete kohli;
+    
+    return 0;
+}
