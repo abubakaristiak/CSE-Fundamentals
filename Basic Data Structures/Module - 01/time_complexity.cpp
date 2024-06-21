453b7f77bda66e07def3f5f7c7a3be532438d396
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-06-21 18:41:51 +0600

    modified the program

--- Basic Data Structures/Module - 01/time_complexity.cpp
+++ Basic Data Structures/Module - 01/time_complexity.cpp
@@ -1,13 +1,43 @@
+// 1
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for(int i=0; i<n; i++){
+//         for(int j=0; j<i; j++){
+//             cout << "hello world!"<<endl;
+//         }
+//     }
+//     return 0;
+// }
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     int p=0; 
+//     for(int i=1; p<=n; i++){
+//         p = p+i;
+//     }
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
     int n;
     cin >> n;
-    for(int i=0; i<n; i++){
-        for(int j=0; j<i; j++){
-            cout << "hello world!"<<endl;
-        }
+    int p=0;
+    for(int i=0; i<n; i=i*2){
+        p++;
+    }
+    for(int j=1; j<p; j=j*2){
+
     }
     return 0;
 }
