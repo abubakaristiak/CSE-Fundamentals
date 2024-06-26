db0b05301fc2a22bacaffd843a5c4f65b484aead
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-06-26 10:22:12 +0600

    Linked list relatad

--- Basic Data Structures/Module -6.5/_04.cpp
+++ Basic Data Structures/Module -6.5/_04.cpp
@@ -47,6 +47,7 @@ void insert_at_position(Node *head, int pos, int v)
 {
     Node *newNode = new Node(v);
     Node *tmp = head;
+
     for(int i=1; i<=pos-1; i++){
         tmp = tmp->next;
         if(tmp == NULL){
