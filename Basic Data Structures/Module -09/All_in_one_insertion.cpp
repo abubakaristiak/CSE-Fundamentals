02d91d98e432da8c16230e993355133ded9af37a
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-06-30 22:10:04 +0600

    doubly linked list

--- Basic Data Structures/Module -09/All_in_one_insertion.cpp
+++ Basic Data Structures/Module -09/All_in_one_insertion.cpp
@@ -1,11 +1,11 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 class Node
 {
-public:
-    int val;
-    Node *next;
-    Node *prev;
+    public:
+        int val;
+        Node* next;
+        Node* prev;
     Node(int val)
     {
         this->val = val;
@@ -13,39 +13,103 @@ public:
         this->prev = NULL;
     }
 };
-
-void print_normal(Node *head)
-{
+void print_normal(Node *head){
     Node *tmp = head;
     while (tmp != NULL)
     {
-        cout << tmp->val << " ";
+        cout << tmp->val <<" ";
         tmp = tmp->next;
     }
     cout << endl;
+    
 }
-
-void print_reverse(Node *tail)
-{
+void print_reverse(Node *tail){
     Node *tmp = tail;
     while (tmp != NULL)
     {
-        cout << tmp->val << " ";
+        cout << tmp->val <<" ";
         tmp = tmp->prev;
     }
     cout << endl;
+    
 }
-
-void insert_at_position(Node *head, int pos, int val)
-{
+void insert_at_position(Node *head, int pos, int val){
     Node *newNode = new Node(val);
     Node *tmp = head;
-    for (int i = 1; i <= pos - 1; i++)
-    {
+    for(int i=1; i<=pos-1; i++){
         tmp = tmp->next;
     }
-    newNode->next = tmp->next;     // 100->30
-    tmp->next = newNode;           // 20->100
-    newNode->next->prev = newNode; // 100<-30
-    newNode->prev = tmp;           // 20<-100
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+    newNode->next->prev = newNode;
+    newNode->prev = tmp;
+    
+}
+int size(Node *head){
+    int count = 0;
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        count++;
+        tmp = tmp->next;
+    }
+    return count;
+    
+}
+void insert_head(Node * &head, Node *&tail, int val){
+    Node *newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    newNode->next = head;
+    head->prev = newNode;
+    head = newNode;
+}
+void insert_tail(Node *& head, Node * &tail, int val){
+    Node *newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    newNode->prev = tail;
+    tail = tail->next;
+}
+int main()
+{
+    Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* b = new Node(30);
+    Node* c = new Node(40);
+    Node* tail = c;
+    // connection
+    head->next=a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+    b->next = c;
+    c->prev = b;
+    
+    int pos, val;
+    cin >> pos >> val;
+    if(pos > size(head)){
+        cout << "Invaild"<<endl;
+    }
+    else if(pos==0){
+        insert_head(head,tail, val);
+    }
+    else if(pos == size(head)){
+        insert_tail(head, tail, val);
+    }
+    else{
+        insert_at_position(head, pos, val);
+    }
+    
+    print_normal(head); 
+    print_reverse(tail);
+    
+    return 0;
 }
