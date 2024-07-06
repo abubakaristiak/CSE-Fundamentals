98938556b51ba2e3ff68fd5294de606f9061be91
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-07-07 00:50:57 +0600

    concept of stack & use of STL

---
+++ Basic Data Structures/Module -13/_01_stack_using_array.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+class myStack{
+    public:
+        vector<int> v;
+        void push(int val){
+            v.push_back(val);
+        }
+        void pop(){
+            v.pop_back();
+        }
+        int top(){
+            return v.back();
+        }
+        int size(){
+            return v.size();
+        }
+        bool empty(){
+            if(v.size() == 0) return true;
+            else return false;
+        }
+};
+int main()
+{
+    myStack st;
+    st.push(10);
+    st.push(20);
+    cout<<st.top()<<endl;
+    st.pop();
+    // cout <<st.top() <<endl;
+    // st.pop();
+    if(st.empty() == false){
+        cout << st.top()<<endl;
+    }
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -1,61 +1,100 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// class Node
-// {
-//     public:
-//         int val;
-//         Node* next;
-//     Node(int val)
-//     {
-//         this->val = val;
-//         this->next = NULL;
-//     }
-// };
-
-// void print_recursion(Node *n){
-//     if(n==NULL) return;
-//     print_recursion(n->next);
-//     cout << n->val <<" ";
-// }
-// void reverse(Node *&head, Node *run){
-//     if(run->next == NULL){
-//         head = run;
-//         return;
-//     }
-//     reverse(head, run->next);
-//     run->next->next=run;
-//     run->next = NULL;
-// }
-// void print(Node *head){
-//     Node *tmp = head;
-//     while (tmp != NULL)
-//     {
-//         cout<<tmp->val << " ";
-//         tmp = tmp->next;
-//     }
-//     cout << endl;
-    
-// }
-// int main()
-// {
-//     Node *head = new Node(10);
-//     Node *a = new Node(20);
-//     Node *b = new Node(30);
-
-//     head->next = a;
-//     a->next = b;
-//     // print_recursion(head);
-//     print(head);
-//     reverse(head, head);
-//     print(head);
-//     return 0;
-// }
 #include<bits/stdc++.h>
 using namespace std;
+class Node{
+    public:
+        int val;
+        Node *next;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+
+};
+void insert_tail(Node *&head, Node *&tail, int val){
+    Node *newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = newNode;
+
+}
+void printList(Node *head){
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val <<" ";
+        tmp = tmp->next;
+    }
+    cout << endl;
+    
+}
+// void check_duplicate(Node *head){
+//     Node *tmp = head;
+//     Node *tmp2 = head->next;
+//     while (tmp != NULL)
+//     {
+//         while (tmp2 != NULL)
+//         {
+//             if(tmp->next->val == tmp2->next->val){
+//                 tmp = tmp->next->next;
+//             }
+//             tmp2 = tmp2->next;
+//         }
+//         tmp = tmp->next;
+        
+//     }
+    
+// };
+void check_duplicate(Node *head){
+    if(head == NULL) return;
+    Node *tmp = head;
+    while (tmp != NULL && tmp != NULL)
+    {
+        Node *tmp2 = tmp;
+        while (tmp2->next != NULL)
+        {
+            if(tmp->val == tmp2->next->val){
+                Node *duplicate = tmp2->next;
+                tmp->next = tmp->next->next;
+                delete duplicate;
+            }
+            else{
+                tmp2 = tmp2->next;
+            }
+            
+        }
+        while (head != NULL && head->next != NULL && head->val == head->next->val)
+        {
+            Node *duplicate = head;
+            head = head->next;
+            delete duplicate;
+        }
+        
+        tmp = tmp->next;
+        
+    }
+    
+};
 int main()
 {
-    vector<int> a;
-    int b; cin>> b;
-    a.push_back(b);     
+    Node *head = NULL;
+    Node *tail = NULL;
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if(val == -1){
+            break;
+        }
+        insert_tail(head, tail, val);
+    }
+    check_duplicate(head);
+    printList(head);
+    
+    
+
     return 0;
 }
