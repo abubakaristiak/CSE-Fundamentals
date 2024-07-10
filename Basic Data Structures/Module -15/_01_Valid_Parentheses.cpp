bf7ee3a84ab0d1dfbb7ddad5a4755e7c4c8540be
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-07-10 19:01:27 +0600

    add leetcode & coding ninjas problem stack & queue related

---
+++ Basic Data Structures/Module -15/_01_Valid_Parentheses.cpp
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/valid-parentheses/
+/*
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c:s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                else{
+                    if(c==')' && st.top()=='('){
+                        st.pop();
+                    }
+                    else if(c=='}' && st.top()=='{'){
+                        st.pop();
+                    }
+                    else if(c==']' && st.top()=='['){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+
+                }
+            }
+        }
+        return st.empty(); 
+    }
+};
+*/
