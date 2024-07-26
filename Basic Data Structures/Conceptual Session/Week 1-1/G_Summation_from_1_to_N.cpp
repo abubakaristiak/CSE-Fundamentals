64ecd71282842c76794258134a5c4a3d5346b503
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-07-26 21:38:36 +0600

    DS conceptual session

---
+++ Basic Data Structures/Conceptual Session/Week 1-1/G_Summation_from_1_to_N.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+#define nl '\n'
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long int n;
+    cin >> n;
+    long long int sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+    
+    for (int i = 1; i <= n; i++)
+    {
+       for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+    }
+
+    
+    sum = (n*(n+1))/2;
+    cout<<sum<<endl;
+
+    return 0;
+}
