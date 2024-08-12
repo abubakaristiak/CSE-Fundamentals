cf4cde14a7b5d65869749d472b10acbfc813a8d9
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-08-12 18:30:00 +0600

    recap session singly linked list

--- Basic Data Structures/Conceptual Session/Recap Session/Week 2/_01_insert.cpp
+++ Basic Data Structures/Conceptual Session/Recap Session/Week 2/_01_insert.cpp
@@ -1,6 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-class Node 
+class Node
 {
 public:
     int val;
@@ -8,39 +8,81 @@ public:
     Node(int val)
     {
         this->val = val;
-        this->next = next;
+        this->next = NULL;
     }
 };
-void printList(Node *head)
+void insertTail(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+    Node *tmp = head;
+    while (tmp->next != NULL)
+    {
+        tmp = tmp->next;
+    }
+    tmp->next = newNode;
+    
+}
+void insertHead(Node *&head, int val)
+{
+    Node *newNode = new Node(val);
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+    newNode->next = head;
+    head = newNode;
+}
+void insertAnyPos(Node *head, int pos, int val){
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+    for(int i=1; i<pos-1; i++)
+    {
+        tmp = tmp->next;
+    }
+    newNode->next = tmp->next;
+    tmp->next = newNode;
+}
+void printingList(Node *head)
 {
     Node *tmp = head;
     while (tmp != NULL)
     {
-        cout << tmp->val <<" ";
-        tmp=tmp->next;
+        cout << tmp->val << " ";
+        tmp = tmp->next;
     }
     
-}
-void insert_tail(Node *head, int val)
-{
-    Node *newNode = new Node(val);
-    if(head == NULL){
-        newNode = head;
-        return;
-    }
-    head->next = newNode;
 }
 int main()
 {
     Node *head = NULL;
-    int val;
+    int val; 
+    cout << "Enter value for insert tail: "<<endl;
     while (true)
     {
         cin >> val;
         if(val == -1) break;
-        insert_tail(head, val);
+        insertTail(head, val);
     }
+    cout << "Inserted at tail -> ";
+    printingList(head);
+    cout << endl;
+
+    cout << "Enter value for insert head: "<<endl;
+    cin >> val;
+    insertHead(head, val);
+    cout << "Inserted head -> ";
+    printingList(head);
+
+    cout << endl << "Enter value for insert any position: "<<endl;
+    int val2, pos;
+    cin >> pos >> val2;
+    insertAnyPos(head, pos, val2);
+    cout << "Inserted any position -> ";
+    printingList(head);
     
-    printList(head);
     return 0;
 }
