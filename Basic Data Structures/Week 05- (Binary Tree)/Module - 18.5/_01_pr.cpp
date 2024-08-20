15442629d6606b7cabee49ec0512839d280d4a12
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-08-20 23:15:57 +0600

    binary tree related problem in Coding Ninjas

---
+++ Basic Data Structures/Week 05- (Binary Tree)/Module - 18.5/_01_pr.cpp
@@ -0,0 +1,20 @@
+// TODO: https://www.codingninjas.com/studio/problems/name_2035933
+
+/*
+vector<int> v;
+void preOrder(TreeNode* root)
+{
+    if(root == NULL) return;
+    preOrder(root-> left);
+    preOrder(root-> right);
+    v.push_back(root-> data);
+
+}
+vector<int> postorderTraversal(TreeNode *root)
+{
+    // Write your code here.
+    v.clear();
+    preOrder(root);
+    return v;
+}
+*/
