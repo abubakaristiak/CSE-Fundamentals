435d01898b64d6e3c6dac74911c5eb1a71df7311
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-08-21 00:39:09 +0600

    binary tree related problem in Coding Ninjas

---
+++ Basic Data Structures/Week 05- (Binary Tree)/Module - 18.5/_02_pr.cpp
@@ -0,0 +1,17 @@
+//TODO: https://www.codingninjas.com/studio/problems/preorder-binary-tree_5948
+/*
+vector<int> v;
+void pre_Order(TreeNode<int>* root)
+{
+    if(root == NULL) return;
+    v.push_back(root->data);
+    pre_Order(root->left);
+    pre_Order(root->right);
+}
+vector<int> preOrder(TreeNode<int> * root){
+    // Write your code here.
+    v.clear();
+    pre_Order(root);
+    return v;
+}
+*/
