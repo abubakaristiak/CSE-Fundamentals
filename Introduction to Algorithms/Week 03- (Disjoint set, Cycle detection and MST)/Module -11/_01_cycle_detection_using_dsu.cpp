28d45e8d8c42f2985aa5e5934495d581cdbbb229
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-09-19 15:47:46 +0600

    minimum spanning tree and cycle detection

---
+++ Introduction to Algorithms/Week 03- (Disjoint set, Cycle detection and MST)/Module -11/_01_cycle_detection_using_dsu.cpp
@@ -0,0 +1,75 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int n = 1e5+5;
+int parent[n];
+int group_size[n];
+
+void dsu_initialize(int n){
+    for(int i=0; i<n; i++){
+        parent[i] = -1;
+        group_size[i] = 1;
+    }
+}
+
+int dsu_find(int node){
+    if(parent[node] == -1){
+        return node;
+    }
+    int leader = dsu_find(parent[node]);
+    parent[node] = leader;
+    return leader;
+}
+
+void dsu_union_by_size(int node1, int node2){
+    int leaderA = dsu_find(node1);
+    int leaderB = dsu_find(node2);
+
+    if(group_size[leaderA] > group_size[leaderB]){
+        parent[leaderB] = leaderA;
+        group_size[leaderA] += group_size[leaderB];
+    }
+    else{
+        parent[leaderA] = leaderB;
+        group_size[leaderB] += group_size[leaderA];
+    }
+}
+
+int main()
+{
+    int n,m;
+    cin >> n >> m;
+    dsu_initialize(n);
+    bool cycle;
+    while (m--)
+    {
+        int a,b;
+        cin >> a >> b;
+        int leaderA = dsu_find(a);
+        int leaderB = dsu_find(b);
+
+        if(leaderA == leaderB){
+            cycle = true;
+        }
+        else{
+            dsu_union_by_size(a,b);
+        }
+        
+    }
+    if(cycle) cout << "cycle found" << endl;
+    else cout << "cycle not found" << endl;
+    
+    return 0;
+}
+
+
+/*
+input: 
+6 7
+0 3
+0 1
+0 2
+0 1
+3 5
+3 4
+5 4
+*/
