8512d3a74f51c4b170fa09efd873f1cea9cc9d3b
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-09-19 22:59:08 +0600

    dsu related problems

--- Introduction to Algorithms/Conseptual Session/Week 3-2/_01_Road_Construction.cpp
+++ Introduction to Algorithms/Conseptual Session/Week 3-2/_01_Road_Construction.cpp
@@ -1,14 +1,17 @@
-    #include<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-const int n = 100005;
-int par[n];
-int sz[n];
+const int N = 1e5+5;
+int par[N];
+int sz[N];
+int cmp, mx;
 
 void dsu_init(int n){
     for(int i=0; i<n; i++){
         par[i] = -1;
         sz[i] = 1;
     }
+    mx = INT_MIN;
+    cmp = n;
 }
 
 int dsu_find(int node){
@@ -16,37 +19,53 @@ int dsu_find(int node){
         return node;
     }
     int leader = dsu_find(par[node]);
-    return leader; 
+    par[node] = leader;
+    return leader;
 }
 
-void Union_by_size(int nodeA, int nodeB){
+void dsu_union_by_size(int nodeA, int nodeB){
     int leaderA = dsu_find(nodeA);
     int leaderB = dsu_find(nodeB);
+
+    if(leaderA == leaderB) {
+        return;
+    }
 
     if(sz[leaderA] > sz[leaderB]){
         par[leaderB] = leaderA;
         sz[leaderA] += sz[leaderB];
+        mx = max(mx, sz[leaderA]);
     }
     else{
         par[leaderA] = leaderB;
-        sz[leaderB] += sz[leaderA]; 
+        sz[leaderB] += sz[leaderA];
+        mx = max(mx, sz[leaderB]);
     }
+    cmp--;
 }
-
-
-
 
 int main()
 {
     int n, m;
     cin >> n >> m;
+    dsu_init(n);
+
     while (m--)
     {
-        int a, b;
+        int a,b;
         cin >> a >> b;
-        Union_by_size(a,b);
+        dsu_union_by_size(a,b);
+        cout << cmp << " " << mx << endl;
+
     }
     
     return 0;
 }
 
+/*
+input:
+5 3
+1 2
+1 3
+4 5
+*/
