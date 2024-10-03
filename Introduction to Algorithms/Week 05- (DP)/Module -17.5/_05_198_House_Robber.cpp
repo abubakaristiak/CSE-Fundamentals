30867b1c1473f849fed08eff870935207e76abc5
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-10-03 21:06:31 +0600

    Dynamic programming

---
+++ Introduction to Algorithms/Week 05- (DP)/Module -17.5/_05_198_House_Robber.cpp
@@ -0,0 +1,25 @@
+//https://leetcode.com/problems/house-robber/description/
+
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int dp[105];
+    int rec(int n, vector<int>& nums)
+    {
+        if(n<0) return 0;
+        if(dp[n] != -1) return dp[n];
+
+        int op1 = nums[n] + rec(n-2, nums);
+        int op2 = rec(n-1, nums);
+        return dp[n] = max(op1, op2);
+    }
+
+
+    int rob(vector<int>& nums) 
+    {
+        memset(dp, -1, sizeof(dp));
+        int n = nums.size();
+        return rec(n-1,nums);
+    }
+};
